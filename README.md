fa1c1564ffc33bd91c62bf8c355f69747515bd57
João Pedro Schmitt <schmittjoaopedro@gmail.com>
2016-08-08 22:41:37 -0300

    add partial files of the cleaning data of coursera

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# getting-and-cleaning-data
-Getting and cleaning data - peer graded test
+# Getting and cleaning data - Peer graded test
+
+## Steps to run the software
+
+This file describe the steps to tidy the data collected of the movements of sensors from Smartphones.
+
+1. Have installed properly the software R and the dplyr library.
+2. So start the R software, copy the path of the directory where this github repository was cloned
+3. Using setwd() command, define the directory copied in step 2.
+4. Check your directory access to write operations (permission to write)
+5. Load and run the script run_analysis.R with the command: 
+    ```R
+    source("run_analysis.R")
+    ```
+
+# What the script *run_analysis.R" makes
+
+- First the script load the features.txt and normalize their values
+- After the script filter only mean and std values of the features.txt
+- So the script loads the values of the mean and std of the training and test of the datasets X
+- Then the script merges the training and test datasets of X with the datasets of Y and customers
+- So the script replace the activity Id by the activity label
+- Then the script calculates the mean of the values of each column for each group
+- Finally the script writes the calculated dataset in a file called tidy_data.txt
+
+Author: João Pedro Schmitt
--- run_analysis.R
+++ run_analysis.R
@@ -1,59 +1,83 @@
-#Colocar library(dplyr)
-#Colocar merge_data_sets na chamada principal
+#
+# Author: João Pedro Schmitt
+#
+# Script used to load raw data from measurements of smartphones and 
+# tidy the data and calculate the mean and std for each group formed by
+# acvitity and customers
+#
+# Available at: https://github.com/schmittjoaopedro/getting-and-cleaning-data
+#
 
-run_analysis <- function() {
-    
-    #merge_data_sets();
-    
-	# Defines all data directories
-	baseDir <- "./data/"
-	outputDir <- paste(baseDir, "output/", sep = "")
-    
-    if(!file.exists(outputDir)) dir.create(outputDir)
 
-    # Reads the activities labels
-    activities <- read.table(paste(baseDir, "activity_labels.txt", sep = ""), col.names = c("id","activity"))
-    sensorlabels <- read.table(paste(baseDir, "features.txt", sep = ""), col.names = c("id","features"))
-    
-    rawX <- read.table(paste(outputDir, "X_raw.txt", sep = ""), col.names = sensorlabels$features)
-    rawSubject <- read.table(paste(outputDir, "subject_raw.txt", sep = ""), col.names = "subject")
-    rawY <- read.table(paste(outputDir, "y_raw.txt", sep = ""), col.names = "activityid")
-    rawY <- rawY %>% mutate(activitylabel = activities[activityid,]$activity)
-    rawY <- cbind(rawY, rawSubject, rawX)
-    rawX <- rawY[,2:dim(rawY)[2]]
-    rawX <- rawX[,grepl("activitylabel|subject|-mean()|-std()", names(rawX))]
-    names(rawX) <- gsub("\\.","", tolower(names(rawX)))
-    write.csv(rawX, file = paste(outputDir, "tidy_data.csv", sep = ""), col.names = T, row.names = F)
-    
+# Import the required libraries
+library(dplyr)
+
+
+
+# STEP 1 - OBTAIN THE FILES
+# ==========================================================================================================================
+# Create util variables
+fileName <- "data.zip"
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+dataPath <- "UCI HAR Dataset"
+
+# Manage dataset folders and files
+if(!file.exists(fileName)) {
+    download.file(url = fileURL, destfile = fileName, method = "curl")
 }
 
-merge_data_sets <- function () {
-    
-    baseDir <- "./data/"
-    trainDir <- paste(baseDir, "train/", sep = "")
-    testDir <- paste(baseDir, "test/", sep = "")
-    outputDir <- paste(baseDir, "output/", sep = "")
-    
-    if(!file.exists(outputDir)) dir.create(outputDir)
-    
-    trainX <- read.table(paste(trainDir, "X_train.txt", sep = ""), colClasses = "character")
-    testX <- read.table(paste(testDir, "X_test.txt", sep = ""), colClasses = "character")
-    write.table(rbind(trainX, testX), file = "./data/output/X_raw.txt", col.names = F, row.names = F, quote = F)
-    
-    trainY <- read.table(paste(trainDir, "y_train.txt", sep = ""), colClasses = "character")
-    testY <- read.table(paste(testDir, "y_test.txt", sep = ""), colClasses = "character")
-    write.table(rbind(trainY, testY), file = "./data/output/y_raw.txt", col.names = F, row.names = F, quote = F)
-    
-    trainSubject <- read.table(paste(trainDir, "subject_train.txt", sep = ""), colClasses = "character")
-    testSubject <- read.table(paste(testDir, "subject_test.txt", sep = ""), colClasses = "character")
-    write.table(rbind(trainSubject, testSubject), file = "./data/output/subject_raw.txt", col.names = F, row.names = F, quote = F)
+# Unzip the downloaded file
+if(!file.exists(dataPath)) {
+    unzip(zipfile = fileName)
 }
 
 
 
 
+# STEP 2 - LOAD AND PREPARE THE RAW DATASET
+# ==========================================================================================================================
+
+# Load the activity_labels
+activitylabels <- read.table(file.path(dataPath,"activity_labels.txt"), col.names = c("activityId","activityName"))
+
+# Load the features, extract only the means and std values of the data set and normallize the names of the measurements
+features <- read.table(file.path(dataPath, "features.txt"), col.names = c("featureId","featureCode"))
+featuresToSelect <- grepl(".mean.|.std.", features$featureCode) # Extract only the names with mean or std
+features <- features[featuresToSelect,2] # Select the labels
+features <- gsub("[()]|-","",gsub("mean","Mean",gsub("std", "Std", features))) # Standardize the names
+
+# Load and merge the test and train datasets. Select only the mean and std columns for the dataset of the values,
+# and bind columns of activity, subjects and values
+# Do only for test
+testY <- read.table(file.path(dataPath, "test", "y_test.txt")) # Load activity
+testX <- read.table(file.path(dataPath, "test", "X_test.txt"))[,featuresToSelect] # Load values of mean and std
+testSub <- read.table(file.path(dataPath, "test", "subject_test.txt")) # Load subjects
+testData <- cbind(testY, testSub, testX) # Bind the columns
+# Do only for train
+trainY <- read.table(file.path(dataPath, "train", "y_train.txt")) # Load activity
+trainX <- read.table(file.path(dataPath, "train", "X_train.txt"))[,featuresToSelect] # Load values of mean and std
+trainSub <- read.table(file.path(dataPath, "train", "subject_train.txt")) # Load subjects
+trainData <- cbind(trainY, trainSub, trainX) # Bind the columns
+
+# Merge train and test datasets
+dataset.raw <- rbind(testData, trainData)
+# Rename the column names
+names(dataset.raw) <- c("activityId","featureCode", features)
+
+# Add activity label instead use the id of activity, by merging
+dataset.raw <- merge(activitylabels, dataset.raw, by = "activityId")
 
 
 
 
+# STEP 3 - PROCESS THE TIDY DATASET PRODUCED BY STEP 2
+# ==========================================================================================================================
+# Exclude the activityId column, group all data by activityName and featureCode and calulate the mean 
+# for the values of each group
+dataset.tidy <- dataset.raw %>% 
+    select(-(activityId)) %>% 
+    group_by(activityName, featureCode) %>% 
+    summarise_each(funs(mean))
 
+# Create a file with the tidy data
+write.table(dataset.tidy, "tidy_data.txt", row.names = F)
